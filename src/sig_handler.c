34cb2b9212d04ed368f7d14227b4f46b3ef0f709
MasterTextman <mctech26@gmail.com>
2018-05-10 20:20:49 +0200

    removed printf's from signal handler

--- src/sig_handler.c
+++ src/sig_handler.c
@@ -12,27 +12,13 @@ void signal_handler(int signo){
 
     // Catches interrupt signal
     if(signo == SIGINT){
-        printf("\n------------------------------------------------------\n");
-        printf("Interrupting Process [%d] with Signal [%d]...\n", process, SIGINT);
         int success = kill(process, SIGINT);
-        (success == 0)
-          ? printf("Interrupt was successful!\n")
-          : printf("Interrupt failed.\n");
-        printf("------------------------------------------------------\n");
     }
 
     // Catches suspend signal
     else if(signo == SIGTSTP){
-        printf("\n------------------------------------------------------\n");
-        printf("Suspending Process [%d] with Signal [%d]...\n", process, SIGTSTP);
         int success = kill(process, SIGTSTP);
-        (success == 0)
-          ? printf("Suspension was successful!\n")
-          : printf("Suspension failed.\n");
-        printf("------------------------------------------------------\n");
-
         resuspended = 1;
-
         suspended_process = process;
     }
 }
@@ -43,9 +29,6 @@ void resumeProcessSignal(int state){
         return;
     }
     else{
-        printf("\n------------------------------------------------------\n");
-        printf("Resuming process [%d] with Signal [%d]...\n", suspended_process, SIGCONT);
-        printf("------------------------------------------------------\n");
         int resumed = resumeProcess(state, suspended_process);
         if(resumed == 0){
             suspended_process = -1;
@@ -53,8 +36,6 @@ void resumeProcessSignal(int state){
         }
         else{
             if(resuspended == 0){
-                printf("Process [%d] couldn't be resumed.\n", suspended_process);
-                printf("------------------------------------------------------\n");
                 setExitcode(-1);
             }
             else{
